87c94e3d285f6beeb925261c55c23d86e0a5ffe5
albingithubnow <albin_mathew@hotmail.com>
2026-02-25 11:17:05 +0530

    firstcommittolocalbranch1

--- src/test/java/PackageOne/ClassOne.java
+++ src/test/java/PackageOne/ClassOne.java
@@ -1,10 +1,15 @@
 package PackageOne;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class ClassOne {
 
 	public static void main(String[] args) {
 
-		WebDrive
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https:www.google.com");
 	}
 
 }
